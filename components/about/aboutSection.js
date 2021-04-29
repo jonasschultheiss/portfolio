3eeee7f7ebbcf6ccbb821f04af7c061c6b6c2129
jonasschultheiss <jonas.raphael.schultheiss@gmail.com>
2021-04-29 19:20:19 +0200

    :lipstick: minimal ui change

--- components/about/aboutSection.js
+++ components/about/aboutSection.js
@@ -5,12 +5,12 @@ import fadeIn from '@variants/fadeIn';
 import { motion } from 'framer-motion';
 
 export default function AboutSection({ image, body, imageRight }) {
-  let classes = 'flex justify-evenly items-center mt-6';
-  classes += imageRight ? ' flex-col xl:flex-row-reverse' : ' flex-col xl:flex-row';
+  let classes = 'flex justify-evenly items-center mt-6 space-x-2';
+  classes += imageRight ? ' flex-col xl:flex-row-reverse' : ' flex-col xl:flex-row ';
   return (
     <motion.div initial="hidden" animate="visible" variants={fadeIn} className={classes}>
       <CDNImage image={image} width={600} height={400} className="rounded" layout="intrinsic" />
-      <div className=" md:max-w-3xl mt-4 md:ml-4 xl:mb-0 mb-4 xl:text-left text-left md:text-center">
+      <div className="md:max-w-3xl mt-4 xl:my-0 mb-4 xl:text-left text-left md:text-center">
         <SanityBlockContent blocks={body} serializers={blockSerializer} />
       </div>
     </motion.div>
